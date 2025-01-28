c3c93a9bbf24b3839e26797b61016bdfb0fb1d51
nataliafrancisca <nathifrancisca@gmail.com>
2025-01-27 23:27:59 -0300

    feat: create the methods to add friend to the list and draw a friend

--- app.js
+++ app.js
@@ -1,1 +1,74 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+let listaAmigosSorteados = [];
+
+function verificaNomesRepetidos(nome){
+    return listaAmigos.includes(nome);
+}
+
+function adicionarAmigo(){
+    const amigo = document.getElementById("amigo");
+    const textoAlerta = document.getElementById("alerta");
+
+    const amigoJaFoiAdicionado = verificaNomesRepetidos(amigo.value);
+
+    if(amigoJaFoiAdicionado){
+        textoAlerta.innerHTML = "Você já adicionou esse nome.";
+        return;
+    }
+
+    textoAlerta.innerHTML = "";
+    listaAmigos.push(amigo.value);
+    imprimirAmigosNaLista(amigo.value);
+    amigo.value = "";
+}
+
+function imprimirAmigosNaLista(nome){
+    const listaAmigosElemento = document.getElementById("listaAmigos");
+
+    const amigo = document.createElement("li");
+    amigo.innerHTML += nome;
+
+    listaAmigosElemento.appendChild(amigo);
+}
+
+function sortearAmigo(){
+    const textoAlerta = document.getElementById("alerta");
+
+    if(listaAmigos.length < 2){
+        textoAlerta.innerHTML = "Você não adicionou ninguém na lista.";
+        return;
+    }
+
+    textoAlerta.innerHTML = "";
+
+    const numeroAleatorio = Math.floor(Math.random() * listaAmigos.length);
+    const amigoSorteado = listaAmigos[numeroAleatorio];
+
+    if(listaAmigosSorteados.length == listaAmigos.length){
+        textoAlerta.innerHTML = "Todos da lista já foram sorteados.";
+        return;
+    }
+
+    if(listaAmigosSorteados.includes(amigoSorteado)){
+        sortearAmigo();
+        return;
+    }
+
+    imprimirSorteado(amigoSorteado);
+    listaAmigosSorteados.push(amigoSorteado);
+}
+
+function imprimirSorteado(sorteado){
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = sorteado;
+}
+
+
+function resetarSorteio(){
+    const textoAlerta = document.getElementById("alerta");
+
+    listaAmigosSorteados = [];
+    imprimirSorteado("");
+
+    textoAlerta.innerHTML = "";
+}
